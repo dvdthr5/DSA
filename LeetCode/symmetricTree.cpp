755322b7ea898fba185225f8aa534b9ba4d3ee6b
David Glover <david@eduroam-169-233-156-87.ucsc.edu>
2026-02-12 12:03:48 -0800

    easy tree questoins

---
+++ LeetCode/symmetricTree.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void invertTreeRec(TreeNode* root){
+        if (root == NULL){
+            return;
+        }
+        if (root->right == NULL && root->left == NULL){
+            return;
+        }
+        if (root->right == NULL){
+            root->right = root->left;
+            root->left = NULL;
+            invertTreeRec(root->right);
+            return;
+        }
+        if (root->left == NULL){
+            root->left = root->right;
+            root->right = NULL;
+            invertTreeRec(root->left);
+            return;
+        }
+        TreeNode* temp = root->right;
+        root->right = root->left;
+        root->left = temp;
+        invertTreeRec(root->right);
+        invertTreeRec(root->left);
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL){
+            return true;
+        }
+        if (p == NULL | q == NULL){
+            return false;
+        }
+        if (p->val != q->val){
+            return false;
+        }
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        invertTreeRec(root);
+        return root;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        root->right = invertTree(root->right);
+        return isSameTree(root->right, root->left);
+    }
+
+
+};
